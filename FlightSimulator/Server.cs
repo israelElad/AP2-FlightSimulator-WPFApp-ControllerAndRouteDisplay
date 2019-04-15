161d97c28e2428698cab40fba996f842836f497c
Narkis Kremizi <narkis.kremizi@live.biu.ac.il>
2019-04-15 10:21:49 +0300

    delete mutex

--- FlightSimulator/Server.cs
+++ FlightSimulator/Server.cs
@@ -17,7 +17,6 @@ namespace FlightSimulator
         private BinaryReader reader;
 
         public bool IsConnected { get; set; }
-        private Mutex mutex;
         public String[] Data { get; set; }
 
         // instance for singleton pattern
@@ -26,7 +25,6 @@ namespace FlightSimulator
         private Server()
         {
             this.IsConnected = false;
-            mutex = new Mutex();
         }
 
         // instance method for singleton pattern
